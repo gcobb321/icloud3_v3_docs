2998764e34d72bd3c591a352ef2c9bf010b07336
gcobb321 <geekstergary@gmail.com>
2025-12-11 15:26:50 -0500

    Update docsify-accordion.js

--- docs/docsify-accordion.js
+++ docs/docsify-accordion.js
@@ -53,7 +53,7 @@
             }
           })
 
-          element.firstChild.textContent = '&bull; '+element.firstChild.textContent.substring(0, element.firstChild.textContent.length - 2);
+          element.firstChild.textContent = '\u2022 '+element.firstChild.textContent.substring(0, element.firstChild.textContent.length - 2);
 
           //Add arrows
           let arrow=document.createElement("i")
